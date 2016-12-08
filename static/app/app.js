707140dc9102cec0d5d682f36db8c265c7a360ca
Ksusha <ksnsher@gmail.com>
2016-12-08 20:04:42 +0200

    new

--- static/app/app.js
+++
@@ -1,4 +0,0 @@
-import angular from "angular";
-import $ from 'jquery';
-var app = angular.module("app", []);
-export {app}
--- static/innit.js
+++
@@ -1,4 +0,0 @@
-import app from './app/app.js';
-import mainCtrl from './app/mainCtrl';
-import feDir from './app/front-end/mainFront-End/front-endDir.js'
-import adminDir from './app/admin/mainAdmin/adminDir.js'
